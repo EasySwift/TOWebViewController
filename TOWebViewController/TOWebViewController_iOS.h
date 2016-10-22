e61f75878c05a6b360fe44546ee93b0f4cad939e
stubbornnessness <1219721814yxj>
2016-10-22 14:24:19 +0800

    2.3版本开始支持carthage

---
+++ TOWebViewController/TOWebViewController_iOS.h
@@ -0,0 +1,17 @@
+//
+//  TOWebViewController_iOS.h
+//  TOWebViewControllerExample
+//
+//  Created by yuanxiaojun on 2016/10/22.
+//  Copyright © 2016年 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for aaa.
+FOUNDATION_EXPORT double TOWebViewController_iOSVersionNumber;
+
+//! Project version string for aaa.
+FOUNDATION_EXPORT const unsigned char TOWebViewController_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TOWebViewController_iOS/PublicHeader.h>
