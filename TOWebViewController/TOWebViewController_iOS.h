d04cf1eeaa362c4fca509eaf3ae3ea44d2bafd43
stubbornnessness <1219721814yxj>
2016-11-01 00:00:04 +0800

    3.0

--- TOWebViewController/TOWebViewController_iOS.h
+++ TOWebViewController/TOWebViewController_iOS.h
@@ -15,3 +15,8 @@ FOUNDATION_EXPORT double TOWebViewController_iOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char TOWebViewController_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TOWebViewController_iOS/PublicHeader.h>
+
+#import "TOWebViewController.h"
+#import "TOActivitySafari.h"
+#import "TOActivityChrome.h"
+#import "UIImage+TOWebViewControllerIcons.h"
